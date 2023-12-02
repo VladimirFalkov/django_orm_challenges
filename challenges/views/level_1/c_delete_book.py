95d7df6fa640a8f53e719109c8a019b762da1017
VladimirFalkov <vladimirfalkov585@gmail.com>
2023-12-02 13:02:11 +0300

    solution for delete_book

--- challenges/views/level_1/c_delete_book.py
+++ challenges/views/level_1/c_delete_book.py
@@ -6,15 +6,20 @@
 После удаления книги попробуйте получить описание удалённой книги с помощью ручки из предыдущего задания
 и убедитесь, что книга удалена.
 """
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponseNotAllowed,
+)
 
 from challenges.models import Book
 from challenges.views.level_1.b_book_details import get_book
 
 
 def delete_book(book_id: int) -> None:
-    # код писать тут
-    pass
+    book = get_book(book_id)
+    book.delete()
 
 
 def delete_book_handler(request: HttpRequest, book_id: int) -> HttpResponse:
